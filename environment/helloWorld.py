49cc47f20943a4a3848dcc6fdc53405d883a836f
Yang Xi <yangxi@Yangs-MBP.modem>
2023-03-08 04:07:36 +1100

    AWS-Restart

---
+++ environment/helloWorld.py
@@ -0,0 +1,17 @@
+"""
+Your module description
+"""
+print("Hello, World")
+print(2 + 2)
+print(4-2)
+print(2*2)
+print(4/2)
+
+print("Python has three numeric types: int, float, and complex")
+
+myValue =1
+print(myValue)
+
+print(type(myValue))
+
+print(str(myValue) + " is of the data type " + str(type(myValue)))
